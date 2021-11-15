1545c0a2c35e4cc89fe040d7e47133a3b273e7b7
Chilarai <chilly5476@gmail.com>
2021-11-15 10:15:19 +0530

    Expo packages and google login basic

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import * as GoogleSignIn from "expo-google-sign-in";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class AuthScreen extends React.Component {
+    state = { user: null };
+
+    componentDidMount() {
+        this.initAsync();
+    }
+
+    initAsync = async () => {
+        await GoogleSignIn.initAsync({
+            // You may ommit the clientId when the firebase `googleServicesFile` is configured
+            // clientId: "<YOUR_IOS_CLIENT_ID>",
+        });
+        this._syncUserWithStateAsync();
+    };
+
+    _syncUserWithStateAsync = async () => {
+        const user = await GoogleSignIn.signInSilentlyAsync();
+        this.setState({ user });
+    };
+
+    signOutAsync = async () => {
+        await GoogleSignIn.signOutAsync();
+        this.setState({ user: null });
+    };
+
+    signInAsync = async () => {
+        try {
+            await GoogleSignIn.askForPlayServicesAsync();
+            const { type, user } = await GoogleSignIn.signInAsync();
+            if (type === "success") {
+                this._syncUserWithStateAsync();
+            }
+        } catch ({ message }) {
+            alert("login: Error:" + message);
+        }
+    };
+
+    onPress = () => {
+        if (this.state.user) {
+            this.signOutAsync();
+        } else {
+            this.signInAsync();
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text onPress={this.onPress}>Toggle Auth</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 200,
+        width: 300,
+    },
 });
