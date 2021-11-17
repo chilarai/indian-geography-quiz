03d3a878f02c15765f1c20994756d683a7ee45c1
Chilarai <chilly5476@gmail.com>
2021-11-17 14:58:13 +0530

    Quiz entries

--- source/QuizScreen.jsx
+++ source/QuizScreen.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import API from "./subcomponents/API";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Column as Col, Row } from "react-native-flexbox-grid";
 
 import * as Constants from "./subcomponents/Constants";
+import { Button, Chip } from "react-native-elements";
 
 class QuizScreen extends React.Component {
     state = {
         counter: 0,
         totalQuizzes: 0,
+        totalRows: 0,
         currentQuiz: [],
         currentRightAnswer: "",
         currentOptions: [],
         currentImage: "",
         optionsSize: 0,
+
+        message: "",
+        icon: "",
+        showSkip: true,
+        showMessage: false,
     };
 
     componentDidMount = async () => {
@@ -24,13 +31,18 @@ class QuizScreen extends React.Component {
 
             var counter = this.state.counter;
 
+            let totalRows =
+                currentQuiz[counter].Options.length / 2 +
+                (currentQuiz[counter].Options.length % 2);
+
             this.setState({
                 currentQuiz: currentQuiz,
-                optionsSize: currentQuiz[counter].Options.size,
+                optionsSize: currentQuiz[counter].Options.length,
+                totalRows: totalRows,
                 currentRightAnswer: currentQuiz[counter].RightOption,
                 currentOptions: currentQuiz[counter].Options,
                 currentImage: currentQuiz[counter].ImageLink,
-                maxSize: currentQuiz.size,
+                totalQuizzes: currentQuiz.length,
             });
         } catch (e) {
             console.log("Error Quiz", e);
@@ -41,55 +53,139 @@ class QuizScreen extends React.Component {
         var counter = this.state.counter;
         counter++;
 
+        var currentQuiz = this.state.currentQuiz;
+
         if (counter < this.state.totalQuizzes) {
             this.setState({
                 currentQuiz: currentQuiz,
-                optionsSize: currentQuiz[counter].Options.size,
+                optionsSize: currentQuiz[counter].Options.length,
                 currentRightAnswer: currentQuiz[counter].RightOption,
                 currentOptions: currentQuiz[counter].Options,
                 currentImage: currentQuiz[counter].ImageLink,
                 counter: counter,
+                showMessage: false,
+                showSkip: true,
             });
         }
+    };
+
+    checkAnswer = async (answer) => {
+        if (answer === this.state.currentRightAnswer) {
+            this.setState({
+                message: "Correct answer",
+                backcolor: "green",
+                icon: "check-circle",
+                showMessage: true,
+                showSkip: false,
+            });
+
+            const categoryID = await AsyncStorage.getItem("@categoryID");
+
+            var options = {
+                SessionKey: Constants.SESSIONKEY,
+                CategoryID: parseInt(categoryID),
+                UserID: Constants.USERID,
+            };
+
+            await API.post("/updatescore", options);
+        } else {
+            this.setState({
+                message:
+                    "Wrong answer. Correct answer is " +
+                    this.state.currentRightAnswer,
+                backcolor: "red",
+                icon: "times-circle",
+                showMessage: true,
+                showSkip: false,
+            });
+        }
+
+        setTimeout(() => {
+            this.nextQuiz();
+        }, 3000);
     };
 
     nextQuiz = () => {
         var counter = this.state.counter;
         counter++;
 
+        var currentQuiz = this.state.currentQuiz;
+
         if (counter < this.state.totalQuizzes) {
             this.setState({
                 currentQuiz: currentQuiz,
-                optionsSize: currentQuiz[counter].Options.size,
+                optionsSize: currentQuiz[counter].Options.length,
                 currentRightAnswer: currentQuiz[counter].RightOption,
                 currentOptions: currentQuiz[counter].Options,
                 currentImage: currentQuiz[counter].ImageLink,
                 counter: counter,
+                showMessage: false,
+                showSkip: true,
             });
         }
     };
 
     render() {
+        let rows = [];
+        for (let i = 0; i < this.state.totalRows; i++) {
+            rows.push(
+                <Row size={12} key={i}>
+                    <Col sm={6} md={6} lg={6}>
+                        <Button
+                            title={this.state.currentOptions[i * 2]}
+                            type="outline"
+                            onPress={() =>
+                                this.checkAnswer(
+                                    this.state.currentOptions[i * 2]
+                                )
+                            }
+                            disabled={this.state.showMessage}
+                        ></Button>
+                    </Col>
+                    <Col sm={6} md={6} lg={6}>
+                        <Button
+                            title={this.state.currentOptions[i * 2 + 1]}
+                            type="outline"
+                            onPress={() =>
+                                this.checkAnswer(
+                                    this.state.currentOptions[i * 2 + 1]
+                                )
+                            }
+                            disabled={this.state.showMessage}
+                        ></Button>
+                    </Col>
+                </Row>
+            );
+        }
+
         return (
             <View>
                 <Image source={this.state.currentImage} />
-                <Row size={12}>
-                    <Col sm={6} md={6} lg={6}>
-                        <Text>{this.state.currentOptions[0]}</Text>
-                    </Col>
-                    <Col sm={6} md={6} lg={6}>
-                        <Text>{this.state.currentOptions[1]}</Text>
-                    </Col>
-                </Row>
+                <View>{rows}</View>
 
-                <Row size={12}>
-                    <Col sm={6} md={6} lg={6}>
-                        <Text>{this.state.currentOptions[2]}</Text>
-                    </Col>
-                    <Col sm={6} md={6} lg={6}>
-                        <Text>{this.state.currentOptions[3]}</Text>
-                    </Col>
-                </Row>
+                {this.state.showSkip ? (
+                    <Button
+                        title="Skip"
+                        type="solid"
+                        onPress={() => this.skipQuiz()}
+                    />
+                ) : (
+                    ""
+                )}
+
+                {this.state.showMessage ? (
+                    <Chip
+                        title={this.state.message}
+                        icon={{
+                            name: this.state.icon,
+                            type: "font-awesome",
+                            size: 20,
+                            color: "white",
+                        }}
+                    />
+                ) : (
+                    ""
+                )}
             </View>
         );
     }
--- source/subcomponents/Constants.jsx
+++ source/subcomponents/Constants.jsx
@@ -1,3 +1,4 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const SESSIONKEY = "asdasd";
+export const USERID = 1;
